d9f1bd9a0e8762694af6c7f4b2473872d12ef592
Jordi Gomis <jgc.jordi@gmail.com>
2019-10-26 20:00:48 +0200

    Updates

---
+++ precache-manifest.1a92b4ef31fb73a7cdd085a142a945ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2faee8ca30d6a9c1ff701105b75eb377",
+    "url": "/jgcjordi/ProyectoToDoReact/index.html"
+  },
+  {
+    "revision": "72ee644d72de9798490e",
+    "url": "/jgcjordi/ProyectoToDoReact/static/css/main.299f690e.chunk.css"
+  },
+  {
+    "revision": "0cccea4e16785043cd6d",
+    "url": "/jgcjordi/ProyectoToDoReact/static/js/2.d3f91ddc.chunk.js"
+  },
+  {
+    "revision": "72ee644d72de9798490e",
+    "url": "/jgcjordi/ProyectoToDoReact/static/js/main.da5310b8.chunk.js"
+  },
+  {
+    "revision": "6749dfd6f046ce49e9a3",
+    "url": "/jgcjordi/ProyectoToDoReact/static/js/runtime-main.43420eff.js"
+  }
+]);
